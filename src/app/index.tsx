20f7dc5fe649df5012ce3d6afb82a37b4e299ce5
Mahmoud Gewily <mahmoud.a.gewily@gmail.com>
2025-04-14 12:46:10 +0300

    feat: add base Redux store configuration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,12 @@
+import { StoreProvider } from "./providers";
+import { AppRouter } from "./router";
+
 function App() {
-  return <></>;
+  return (
+    <StoreProvider>
+      <AppRouter />
+    </StoreProvider>
+  );
 }
 
 export default App;
---
+++ src/app/stores/store/index.ts
@@ -0,0 +1,5 @@
+import { store, AppDispatch, RootState } from "./store";
+
+export type { RootState, AppDispatch };
+
+export { store };
