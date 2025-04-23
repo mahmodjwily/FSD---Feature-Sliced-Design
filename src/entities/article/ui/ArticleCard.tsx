178628c293b27c69de1528bb7409d72913e92139
Mahmoud Gewily <mahmoud.a.gewily@gmail.com>
2025-04-23 16:55:48 +0300

    fix: navigate on title click

--- src/entities/article/ui/ArticleCard.tsx
+++ src/entities/article/ui/ArticleCard.tsx
@@ -12,7 +12,7 @@ export const ArticleCard = ({ article, children }: IArticleCardProps) => {
   return (
     <div className="p-6 bg-white shadow rounded-lg">
       <h2 className="text-2xl font-bold">
-        <Link to={`/article/${article.id}`}>{article.title}</Link>
+        <Link to={`/articles/${article.id}`}>{article.title}</Link>
       </h2>
       <p className="text-muted-foreground">{article.body.slice(0, 100)}</p>
       <div className="mt-4 flex justify-between items-center">{children}</div>
